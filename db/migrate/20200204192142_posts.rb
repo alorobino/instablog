881189e1d8cf1ef4366db9dc246b02d76ca7b7b8
Robin Shackleton <robinshackleton@Robins-MacBook-Air.local>
2020-02-06 09:48:34 -0500

    first commit

---
+++ db/migrate/20200204192142_posts.rb
@@ -0,0 +1,10 @@
+class Posts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.integer :user_id
+      t.string :caption
+
+      t.timestamps
+    end 
+  end
+end
